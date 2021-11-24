b529a969982649a91cd7a0abcdad26363d862574
ashaa9121 <ashaa9121@gmail.com>
2021-11-24 10:56:59 +0530

    random numbers

--- lab7.py
+++ lab7.py
@@ -1,4 +1,5 @@
-# Program :  Write one line of Python that takes a list and makes a new list that has only
+# Program :  List comprehension
+# Write one line of Python that takes a list and makes a new list that has only ,
 #  the even elements of the list in it.
 
 a =  [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
---
+++ lab8.py
@@ -0,0 +1,18 @@
+# Program : Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, 
+# then tell them whether they guessed too low, too high, or exactly right
+
+import random
+
+#print(random.randrange(1,9))
+
+number = random.randrange(1,9)
+guess_a_number = int(input("Please guess a number between 1 and 9:"))
+
+if(guess_a_number not in range(1,9)):
+    print("Only between 1 and 9 is allowed!")
+elif(number == guess_a_number):
+    print("You have guessed exactly right number!!")
+elif(number<guess_a_number):
+    print("You have guessed too high number") 
+elif(number>guess_a_number):
+    print("You have guessed too low number")
