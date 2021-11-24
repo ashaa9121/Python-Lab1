8267cbe330417ddd377d3e4b875ac826ec371710
ashaa9121 <ashaa9121@gmail.com>
2021-11-24 10:28:08 +0530

    list comprehension example

---
+++ lab7.py
@@ -0,0 +1,7 @@
+# Program :  Write one line of Python that takes a list and makes a new list that has only
+#  the even elements of the list in it.
+
+a =  [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+new_list = [i for i in a if(i%2 == 0)]
+print(new_list)
